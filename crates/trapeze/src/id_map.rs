511a4f939401fadca458a6ffac34f55d5251a48f
Jorge Prendes <jorge.prendes@gmail.com>
2024-03-03 22:52:39 +0000

    Avoid spawning a task in `IdMapGuard::drop`
    Signed-off-by: Jorge Prendes <jorge.prendes@gmail.com>

--- crates/trapeze/src/id_map.rs
+++ crates/trapeze/src/id_map.rs
@@ -1,7 +1,7 @@
 use std::collections::{BTreeMap, BTreeSet};
-use std::ops::DerefMut as _;
 use std::sync::Arc;
 
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 use tokio::sync::RwLock;
 
 pub struct IdMap<T: Send + Sync> {
@@ -11,6 +11,13 @@ pub struct IdMap<T: Send + Sync> {
 struct IdMapInner<T: Send + Sync> {
     used: BTreeMap<u32, Arc<T>>,
     available: BTreeSet<u32>,
+    rx: UnboundedReceiver<u32>,
+    tx: UnboundedSender<u32>,
+}
+
+pub struct IdMapGuard {
+    id: u32,
+    tx: UnboundedSender<u32>,
 }
 
 impl<T: Send + Sync> Default for IdMap<T> {
@@ -23,14 +30,17 @@ impl<T: Send + Sync> Default for IdMap<T> {
 
 impl<T: Send + Sync> Default for IdMapInner<T> {
     fn default() -> Self {
+        let (tx, rx) = unbounded_channel();
         Self {
             used: Default::default(),
             available: Default::default(),
+            rx,
+            tx,
         }
     }
 }
 
-impl<T: Send + Sync + 'static> Clone for IdMap<T> {
+impl<T: Send + Sync> Clone for IdMap<T> {
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
@@ -38,58 +48,70 @@ impl<T: Send + Sync + 'static> Clone for IdMap<T> {
     }
 }
 
-pub struct IdMapGuard<T: Send + Sync + 'static> {
-    id: u32,
-    map: IdMap<T>,
-}
-
-impl<T: Send + Sync + 'static> IdMap<T> {
-    fn claim_impl(&self, inner: &mut IdMapInner<T>, id: u32, value: T) -> Option<IdMapGuard<T>> {
-        if inner.used.contains_key(&id) {
-            return None;
-        }
-        let value = Arc::new(value);
-        inner.used.insert(id, value.clone());
-        inner.available.remove(&id);
-        Some(IdMapGuard {
-            id,
-            map: self.clone(),
-        })
+impl<T: Send + Sync> IdMap<T> {
+    pub async fn claim(&self, id: impl Into<Option<u32>>, value: T) -> Option<IdMapGuard> {
+        self.inner.write().await.claim(id.into(), value)
     }
 
-    pub async fn claim(&self, id: u32, value: T) -> Option<IdMapGuard<T>> {
-        let mut inner = self.inner.write().await;
-        self.claim_impl(inner.deref_mut(), id, value)
-    }
-
-    pub async fn claim_any(&self, value: T) -> IdMapGuard<T> {
-        let mut inner = self.inner.write().await;
-        let id = match inner.available.pop_first() {
-            Some(id) => id,
-            None => inner.used.last_key_value().map(|(k, _)| k + 2).unwrap_or(1),
-        };
-        self.claim_impl(inner.deref_mut(), id, value).unwrap()
+    pub async fn claim_any(&self, value: T) -> IdMapGuard {
+        // safe because "id: None" means we will always get "Some(guard)" back.
+        self.claim(None, value).await.unwrap()
     }
 
     pub async fn borrow(&self, id: u32) -> Option<Arc<T>> {
-        self.inner.read().await.used.get(&id).cloned()
+        self.inner.read().await.borrow(id)
     }
 }
 
-impl<T: Send + Sync + 'static> IdMapGuard<T> {
+impl<T: Send + Sync> IdMapInner<T> {
+    fn get_any_id(&mut self) -> u32 {
+        if let Some(id) = self.available.pop_first() {
+            id
+        } else if let Some((id, _)) = self.used.last_key_value() {
+            id + 2
+        } else {
+            1
+        }
+    }
+
+    fn recycle(&mut self) {
+        while let Ok(id) = self.rx.try_recv() {
+            self.used.remove(&id);
+            self.available.insert(id);
+        }
+    }
+
+    pub fn claim(&mut self, id: Option<u32>, value: T) -> Option<IdMapGuard> {
+        self.recycle();
+        let id = match id {
+            Some(id) => id,
+            None => self.get_any_id(),
+        };
+        if self.used.contains_key(&id) {
+            return None;
+        }
+        let value = Arc::new(value);
+        self.used.insert(id, value.clone());
+        self.available.remove(&id);
+        Some(IdMapGuard {
+            id,
+            tx: self.tx.clone(),
+        })
+    }
+
+    pub fn borrow(&self, id: u32) -> Option<Arc<T>> {
+        self.used.get(&id).cloned()
+    }
+}
+
+impl IdMapGuard {
     pub fn id(&self) -> u32 {
         self.id
     }
 }
 
-impl<T: Send + Sync + 'static> Drop for IdMapGuard<T> {
+impl Drop for IdMapGuard {
     fn drop(&mut self) {
-        let id = self.id;
-        let map = self.map.clone();
-        tokio::spawn(async move {
-            let mut inner = map.inner.write().await;
-            inner.used.remove(&id);
-            inner.available.insert(id);
-        });
+        let _ = self.tx.send(self.id);
     }
 }
