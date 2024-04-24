4bf53ad4ddfe41a36c6559617e5ec3501094f736
Jorge Prendes <jorge.prendes@gmail.com>
2024-04-24 14:16:19 +0100

    Remove seal from generated traits
    Signed-off-by: Jorge Prendes <jorge.prendes@gmail.com>

--- crates/trapeze/src/service.rs
+++ crates/trapeze/src/service.rs
@@ -9,16 +9,6 @@ pub trait Service: Send + Sync {
     fn methods(self: Arc<Self>) -> Vec<(&'static str, Arc<dyn MethodHandler + Send + Sync>)>;
 }
 
-mod sealed {
-    pub trait Sealed {}
-
-    impl<T: super::Service> Sealed for T {}
-    impl Sealed for crate::Client {}
-}
-
-pub trait Sealed: sealed::Sealed {}
-impl<T: sealed::Sealed> Sealed for T {}
-
 pub struct UnaryMethod<Input, Output, Method> {
     pub(crate) method: Method,
     _phantom: PhantomData<Mutex<(Input, Output)>>,
